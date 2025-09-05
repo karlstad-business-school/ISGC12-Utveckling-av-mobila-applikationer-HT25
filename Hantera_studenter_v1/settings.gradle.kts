b12bd00d7d400d2f535ac1e6839b94af16d31efb
alexvest <78535319+alexvest@users.noreply.github.com>
2025-09-05 09:56:37 +0200

    Startkod F3

---
+++ Hantera_studenter_v1/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Hantera_studenter_v1"
+include(":app")
+ 
