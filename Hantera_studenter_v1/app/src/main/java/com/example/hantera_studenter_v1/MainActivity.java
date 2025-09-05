d5480f79b62d8e1334c68748e0c46bf7f526ddd0
alexvest <78535319+alexvest@users.noreply.github.com>
2025-09-05 12:09:40 +0200

    F3 Slutkod

--- Hantera_studenter_v1/app/src/main/java/com/example/hantera_studenter_v1/MainActivity.java
+++ Hantera_studenter_v1/app/src/main/java/com/example/hantera_studenter_v1/MainActivity.java
@@ -13,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText idET, nameET, pnrET;
+    private Button addBtn;
+    private TextView studentList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,35 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        new Database();
+
+        idET = findViewById(R.id.student_id);
+        nameET = findViewById(R.id.student_name);
+        pnrET = findViewById(R.id.student_pnr);
+        addBtn = findViewById(R.id.add_btn);
+        studentList = findViewById(R.id.student_list);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = Integer.parseInt(idET.getText().toString());
+                String name = nameET.getText().toString();
+                String pnr = pnrET.getText().toString();
+
+                Student s = new Student(id, name, pnr);
+                Database.instance.add(s);
+                studentList.setText(Database.instance.printStudents());
+            }
+        });
+
+
     }
 
+    public void removeStudent(View view) {
+        EditText rID = findViewById(R.id.remove_id);
+        int id = Integer.parseInt(rID.getText().toString());
+
+        Database.instance.remove(id);
+        studentList.setText(Database.instance.printStudents());
+    }
 }
