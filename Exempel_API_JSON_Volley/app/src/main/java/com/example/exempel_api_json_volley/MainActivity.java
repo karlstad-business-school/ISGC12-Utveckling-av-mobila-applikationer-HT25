657f5c40ae5f442019612d0068758dce9940c649
alexvest <78535319+alexvest@users.noreply.github.com>
2025-10-09 10:08:36 +0200

    F9 - Startkod

---
+++ Exempel_API_JSON_Volley/app/src/main/java/com/example/exempel_api_json_volley/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.exempel_api_json_volley;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView cityTV, skyTV, tempTV;
+    private EditText searchET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        cityTV = findViewById(R.id.city_tv);
+        skyTV = findViewById(R.id.sky_tv);
+        tempTV = findViewById(R.id.temp_tv);
+        searchET = findViewById(R.id.serach);
+    }
+
+
+    public void search(View view){
+
+    }
+
+}
